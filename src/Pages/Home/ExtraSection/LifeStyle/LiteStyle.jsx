22aab5d5dea0b6a1507b967c679bcd9c057d01ab
abdulaliarafat <abdulaliarafat@gmail.com>
2025-08-09 16:04:33 +0600

    extra section  optimaization

--- src/Pages/Home/ExtraSection/LifeStyle/LiteStyle.jsx
+++ src/Pages/Home/ExtraSection/LifeStyle/LiteStyle.jsx
@@ -9,7 +9,7 @@ const LiteStyle = () => {
     return (
        <div
             className='max-w-6xl mx-auto mb-10 my-18'>
-            <h1 className='font-bold text-2xl md:text-3xl mt-8 text-amber-500 mb-10 text-center'>Food effect on LifeStyle</h1>
+            <h1 className='font-semibold text-2xl md:text-3xl mt-8 text-amber-500 mb-10 text-center'>Food effect on LifeStyle</h1>
             <motion.div
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
